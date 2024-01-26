3fb873b92187280b10ed2f3a4f6e9d917b10a528
CerenStrange <xcerenxcelikx@gmail.com>
2024-01-26 19:50:37 +0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,30 @@
+import {useState} from 'react'
+
+function Button() {
+
+    const [temperature, setTemperature] = useState(0)
+
+    function handleClick(){
+      console.log("1 celcius eklendi");
+      setTemperature(temperature+1)
+      console.log(temperature);
+    }
+
+  return (
+    <>
+    <h1>SICAKLIK DÖNÜŞTÜRÜCÜ</h1>
+      <br />
+      <h3>Değer giriniz: {temperature} 'C</h3>
+      <br />
+      <button onClick={handleClick}>Celsius</button>
+      <hr />
+      <button onClick={()=>setTemperature(temperature +34)}>Fahrenheit</button>
+      <hr />
+      <button onClick={()=>setTemperature(temperature +273)}>Kelvin</button>
+      <hr />
+      
+      </>
+  )
+}
+
+export default Button
